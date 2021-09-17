3110b10b067f9720d054950df7d62e4344cfc5d7
Aaron Ault <aultac@gmail.com>
2021-09-17 12:24:01 -0400

    Successfully retrieved spreadsheet

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import axios from 'axios';
+import xlsx from 'xlsx';
+
 import Button from 'react-bootstrap/Button';
 
+if (!window.processGithubResponse) {
+  window.processGithubResponse = (data) => {
+    console.log('Callback was called w/ data: ', data);
+  }
+}
+
 function App () {
+
+  const getMasterData = async () => {
+    const result = await axios.get('https://api.github.com/repos/earthhistoryviz/geowhen/contents/data/MasterData.xlsx', {
+      headers: {
+        'Accept': 'application/vnd.github.v3.raw'
+      }
+    });
+    const spreadsheet = xlsx.read(result.data, {type:"array"});
+    console.log('spreadsheet = ', spreadsheet);    
+  };
+
   return (
     <div>
-      <Button>bootstrap test</Button>
+      <Button onClick={getMasterData}>Get the master data</Button>
 
     </div>
   );
