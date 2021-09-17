e5ff44089f80dc8ace036202bf308672c71dc5a3
IbrahimSaeedPurdue <54752803+IbrahimSaeedPurdue@users.noreply.github.com>
2021-09-17 00:01:03 -0400

    Init setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Button from 'react-bootstrap/Button';
+
+function App () {
+  return (
+    <div>
+      <Button>bootstrap test</Button>
+
+    </div>
+  );
+}
+
+export default App;
